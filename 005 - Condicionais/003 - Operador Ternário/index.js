5ee6f0af5c210a4ddbdfcc2eeeba49726035de19
Diego Rodrigues Cardoso <diego.r.cadoso@hotmail.com>
2025-01-21 14:56:47 -0300

    Condicionais

---
+++ 005 - Condicionais/003 - Operador Ternário/index.js
@@ -0,0 +1,21 @@
+const velocidade = 86;
+const warn = 85;
+
+// condicao ? true : false;
+
+// const condicacao = velocidade >= warn ? "Recebeu uma Multa!" : "Continua Andando!"
+
+// console.log(condicacao);
+
+const condicacao = velocidade >= warn ? console.log("Recebeu uma Multa!"): console.log("Continua Andando!");
+
+if (velocidade >= warn) {
+    console.log("Recebeu uma Munta!");
+} else {
+    console.log("Continua Andando!");
+}
+
+
+
+
+
